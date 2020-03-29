9d9474a352e652995a749e8a0a770be1b26d134f
Ben Harris <benjaminharris28@gmail.com>
2020-03-29 17:32:45 -0400

    built out walk page, added detail to walk database

--- src/Routes/WalkPage/WalkPage.js
+++ src/Routes/WalkPage/WalkPage.js
@@ -8,10 +8,41 @@ class WalkPage extends React.Component {
     render() {
         const { walk_id } = this.props.match.params;
         const { walks } = this.context;
+        const selectWalk = walks.find(walk => walk.walk_id == walk_id)
 
 
         return (
-            <div>Hello</div>
+            <div>
+                <div className="walk-page-title">
+                    Walk for {`${selectWalk.dog_name}`}
+                </div>
+                <div className="walk-page-data">
+                    <p>Walk with: {selectWalk.walker_firstname}</p>
+                    <p>Walk Date: {selectWalk.walk_date}</p>
+                </div>
+                <div className="walk-page-address">
+                    Pickup Address:
+                    {' '}
+                    {selectWalk.pickup_address_street_number}
+                    {' '}
+                     {selectWalk.pickup_address_street_name},
+                     {' '}
+                     {selectWalk.pickup_address_city}
+                     {' '}
+                     {selectWalk.pickup_address_province},
+                     {' '}
+                     {selectWalk.pickup_address_postal_code}
+                </div>
+                <div className="walk-controls">
+                    <button>
+                        Cancel Walk
+                    </button>
+                    <button>
+                        Complete Walk
+                    </button>
+                </div>
+            </div>
+
             
         )
     }
