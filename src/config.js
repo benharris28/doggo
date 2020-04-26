97d6ad598e05119f4b592147082d8aa97895fcef
Ben Harris <benjaminharris28@gmail.com>
2020-04-26 16:58:03 -0400

    updated API endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://tranquil-sea-46393.herokuapp.com/api',
     //TOKEN_KEY: 'thingful-client-auth-token',
   }
